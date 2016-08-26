07f1b6e51c351f3f1bf5db8980b835c7550e8584
andrew <andvoz@mail.ru>
2016-08-26 09:11:54 +0400

    empty

---
+++ SearchAvia/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SearchAvia
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
